c0edad432df483a9713db3b0873c03bd705f64e3
Daniel Hunsaker <danhunsaker@gmail.com>
2015-10-25 14:14:00 -0600

    Initial Release
    All features implemented; all tests (adapted from Local adapter) pass
    
    Signed-off-by: Daniel Hunsaker <danhunsaker@gmail.com>

--- src/RedisAdapter.php
+++ src/RedisAdapter.php
@@ -29,6 +29,7 @@ class RedisAdapter extends AbstractAdapter
     {
         $this->redis = $redis;
         $this->setPathPrefix($prefix);
+        $this->redis->hsetnx($this->applyPathPrefix('/'), '.', json_encode(['path' => '', 'type' => 'dir', 'visibility' => 'public', 'timestamp' => time()]));
     }
 
     /**
@@ -36,7 +37,31 @@ class RedisAdapter extends AbstractAdapter
      */
     public function write($path, $contents, Config $config)
     {
+        $info = $this->getPathInfo($path);
         
+        if ($this->ensurePathExists($info['dirname'], $config))
+        {
+            $fileData = [
+                'path' => $info['path'],
+                'type' => 'file',
+                'contents' => $contents,
+                'visibility' => $config->get('visibility', 'public'),
+                'timestamp' => time(),
+            ];
+            
+            if (in_array($this->redis->hset($this->applyPathPrefix($info['dirname']), $info['basename'], json_encode($fileData)), [0, 1]))
+            {
+                return $fileData;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
     }
 
     /**
@@ -44,7 +69,24 @@ class RedisAdapter extends AbstractAdapter
      */
     public function has($path)
     {
+        $info = $this->getPathInfo($path);
         
+        if ($this->redis->exists($this->applyPathPrefix($info['path'])))
+        {
+            // Is Directory...
+            return true;
+        }
+        elseif ($this->redis->exists($this->applyPathPrefix($info['dirname']))
+            && $this->redis->hexists($this->applyPathPrefix($info['dirname']), $info['basename']))
+        {
+            // Is File...
+            return true;
+        }
+        else
+        {
+            // Doesn't Exist...
+            return false;
+        }        
     }
 
     /**
@@ -52,7 +94,25 @@ class RedisAdapter extends AbstractAdapter
      */
     public function read($path)
     {
+        $info = $this->getPathInfo($path);
         
+        if ( ! $this->has($path))
+        {
+            throw new \League\Flysystem\FileNotFoundException("File not found at path: {$info['path']}");
+        }
+        
+        $data = json_decode($this->redis->hget($this->applyPathPrefix($info['dirname']), $info['basename']), TRUE);
+        
+        if ($data['type'] === 'file')
+        {
+            $data['size'] = Util::contentSize($data['contents']);
+            
+            return $data;
+        }
+        else
+        {
+            return '';
+        }
     }
 
     /**
@@ -60,7 +120,26 @@ class RedisAdapter extends AbstractAdapter
      */
     public function getMetadata($path)
     {
+        $info = $this->getPathInfo($path);
         
+        if ( ! $this->has($path))
+        {
+            throw new \League\Flysystem\FileNotFoundException("File not found at path: {$info['path']}");
+        }
+        
+        $metadata = json_decode($this->redis->hget($this->applyPathPrefix($info['dirname']), $info['basename']), TRUE);
+        
+        if ($metadata['type'] === 'file')
+        {
+            $metadata += [
+                'size' => Util::contentSize($metadata['contents']),
+                'mimetype' => Util::guessMimeType($metadata['path'], $metadata['contents']),
+            ];
+            
+            unset ($metadata['contents']);
+        }
+        
+        return $metadata;
     }
 
     /**
@@ -68,7 +147,9 @@ class RedisAdapter extends AbstractAdapter
      */
     public function getSize($path)
     {
+        $metadata = $this->getMetadata($path);
         
+        return isset($metadata['size']) ? Util::map($metadata, ['size' => 'size']) : false;
     }
 
     /**
@@ -76,7 +157,9 @@ class RedisAdapter extends AbstractAdapter
      */
     public function getMimetype($path)
     {
+        $metadata = $this->getMetadata($path);
         
+        return isset($metadata['mimetype']) ? Util::map($metadata, ['mimetype' => 'mimetype']) : ['mimetype' => 'directory'];
     }
 
     /**
@@ -84,7 +167,9 @@ class RedisAdapter extends AbstractAdapter
      */
     public function getTimestamp($path)
     {
+        $metadata = $this->getMetadata($path);
         
+        return isset($metadata['timestamp']) ? Util::map($metadata, ['timestamp' => 'timestamp']) : false;
     }
 
     /**
@@ -92,7 +177,14 @@ class RedisAdapter extends AbstractAdapter
      */
     public function update($path, $contents, Config $config)
     {
-        
+        if ( ! $this->has($path))
+        {
+            return false;
+        }
+        else
+        {
+            return $this->write($path, $contents, $config);
+        }
     }
 
     /**
@@ -100,7 +192,7 @@ class RedisAdapter extends AbstractAdapter
      */
     public function rename($path, $newpath)
     {
-        
+        return $this->copy($path, $newpath) && $this->delete($path);
     }
 
     /**
@@ -108,7 +200,8 @@ class RedisAdapter extends AbstractAdapter
      */
     public function copy($path, $newpath)
     {
-        
+        $data = $this->read($path);
+        return $this->write($newpath, $data['contents'], new Config()) !== FALSE;
     }
 
     /**
@@ -116,7 +209,16 @@ class RedisAdapter extends AbstractAdapter
      */
     public function delete($path)
     {
+        $info = $this->getPathInfo($path);
         
+        if ($this->has($info['path']) && $this->getMetadata($info['path'])['type'] === 'file')
+        {
+            return $this->redis->hdel($this->applyPathPrefix($info['dirname']), $info['basename']) > 0;
+        }
+        else
+        {
+            return false;
+        }
     }
     
     /**
@@ -124,7 +226,29 @@ class RedisAdapter extends AbstractAdapter
      */
     public function createDir($dirname, Config $config)
     {
+        $info = $this->getPathInfo($dirname);
+        $status = [
+            'path' => $info['path'],
+            'type' => 'dir',
+            'visibility' => $config->get('visibility', 'public'),
+            'timestamp' => time(),
+        ];
         
+        if ( ! $this->has($info['path']))
+        {
+            if ( ! $this->ensurePathExists($info['dirname'], $config)
+                || ! ($this->redis->hsetnx($this->applyPathPrefix($info['path']), '.', json_encode($status))
+                    && $this->redis->hsetnx($this->applyPathPrefix($info['dirname']), $info['basename'], json_encode($status))))
+            {
+                $status = false;
+            }
+        }
+        elseif ($this->getMetadata($info['path'])['type'] === 'file')
+        {
+            $status = false;
+        }
+        
+        return $status;
     }
 
     /**
@@ -132,7 +256,31 @@ class RedisAdapter extends AbstractAdapter
      */
     public function listContents($directory = '', $recursive = false)
     {
+        $info = $this->getPathInfo($directory);
         
+        $result = [];
+        
+        if ($this->has($info['path']) && $this->getMetadata($info['path'])['type'] === 'dir')
+        {
+            foreach ($this->redis->hgetall($this->applyPathPrefix($info['path'])) as $name => $data)
+            {
+                if ($name === '.')
+                {
+                    continue;
+                }
+                
+                $data = json_decode($data, TRUE);
+                
+                $result[] = Util::map($data, ['type' => 'type', 'path' => 'path', 'timestamp' => 'timestamp', 'size' => 'size']);
+                
+                if ($recursive === TRUE && $data['type'] === 'dir')
+                {
+                    $result = array_merge($result, $this->listContents($data['path'], TRUE));
+                }
+            }
+        }
+        
+        return $result;
     }
 
     /**
@@ -140,7 +288,35 @@ class RedisAdapter extends AbstractAdapter
      */
     public function deleteDir($dirname)
     {
+        $info = $this->getPathInfo($dirname);
         
+        if ($this->has($info['path']) && $this->getMetadata($info['path'])['type'] === 'dir')
+        {
+            $status = TRUE;
+            
+            foreach (array_reverse($this->listContents($info['path'], true)) as $file)
+            {
+                if ($file['type'] === 'dir')
+                {
+                    $status = $status && $this->deleteDir($file['path']);
+                }
+                else
+                {
+                    $status = $status && $this->delete($file['path']);
+                }
+            }
+            
+            if ($info['path'] !== '/')
+            {
+                $status = $status && ($this->redis->hdel($this->applyPathPrefix($info['dirname']), $info['basename']) > 0);
+            }
+            
+            return $status && ($this->redis->del($this->applyPathPrefix($info['path'])) > 0);
+        }
+        else
+        {
+            return false;
+        }
     }
 
     /**
@@ -148,7 +324,26 @@ class RedisAdapter extends AbstractAdapter
      */
     public function setVisibility($path, $visibility)
     {
+        $info = $this->getPathInfo($path);
         
+        if ($this->has($info['path']))
+        {
+            $data = json_decode($this->redis->hget($this->applyPathPrefix($info['dirname']), $info['basename']), TRUE);
+            $data['visibility'] = $visibility;
+            
+            if (in_array($this->redis->hset($this->applyPathPrefix($info['dirname']), $info['basename'], json_encode($data)), [0, 1]))
+            {
+                return $data;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
     }
     
     /**
@@ -156,6 +351,42 @@ class RedisAdapter extends AbstractAdapter
      */
     public function getVisibility($path)
     {
+        $metadata = $this->getMetadata($path);
         
+        return isset($metadata['visibility']) ? Util::map($metadata, ['visibility' => 'visibility']) : ['visibility' => 'public'];
+    }
+    
+    protected function getPathInfo($path)
+    {
+        $info = Util::pathinfo('/' . Util::normalizePath($path));
+        $info['path'] = ltrim($info['path'], '/');
+        $info['dirname'] = ltrim($info['dirname'], '/');
+        if (empty($info['basename']))
+        {
+            $info['basename'] = '.';
+        }
+        
+        return $info;
+    }
+    
+    protected function ensurePathExists($path, Config $config)
+    {
+        if ($this->has($path))
+        {
+            if ($this->getMetadata($path)['type'] === 'dir')
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            $info = $this->getPathInfo($path);
+            
+            return is_array($this->createDir($info['path'], $config));
+        }
     }
 }
--- tests/RedisAdapterTests.php
+++ tests/RedisAdapterTests.php
@@ -18,23 +18,23 @@ class RedisAdapterTests extends \PHPUnit_Framework_TestCase
 
     public function setUp()
     {
-        $this->adapter = new RedisAdapter(new Client(), 'flysystem:test:');
+        $this->adapter = new RedisAdapter(new Client(), 'flysystem-test:');
         $this->adapter->write('file.txt', 'contents', new Config());
     }
 
-    public function teardown()
+    public function tearDown()
     {
-        foreach (array_reverse($this->adapter->listContents('', true)) as $file)
-        {
-            if ($file['type'] === 'dir')
-            {
-                $this->adapter->deleteDir($file['path']);
-            }
-            else
-            {
-                $this->adapter->delete($file['path']);
-            }
-        }
+        $this->adapter->deleteDir('');
+    }
+    
+    /**
+     * @covers ::write
+     */
+    public function testWrite()
+    {
+        $result = $this->adapter->write('new_file.txt', 'new contents', new Config());
+        $this->assertSame('new contents', $result['contents']);
+        $this->assertFalse($this->adapter->write('file.txt/new_file.txt', 'contents', new Config()));
     }
 
     /**
@@ -54,16 +54,20 @@ class RedisAdapterTests extends \PHPUnit_Framework_TestCase
     public function testCreateDir()
     {
         $result = $this->adapter->createDir('dir/subdir', new Config());
-        $this->assertSame(2, count($result));
+        $this->assertSame(4, count($result));
         $this->assertSame('dir/subdir', $result['path']);
         $this->assertSame('dir', $result['type']);
+        $this->assertSame('public', $result['visibility']);
+        $this->assertTrue(is_int($result['timestamp']));
         $this->assertTrue($this->adapter->has('dir'));
         $this->assertTrue($this->adapter->has('dir/subdir'));
 
         $result = $this->adapter->createDir('dir', new Config());
-        $this->assertSame(2, count($result));
+        $this->assertSame(4, count($result));
         $this->assertSame('dir', $result['path']);
         $this->assertSame('dir', $result['type']);
+        $this->assertSame('public', $result['visibility']);
+        $this->assertTrue(is_int($result['timestamp']));
 
         $this->assertFalse($this->adapter->createDir('file.txt', new Config()));
         $this->assertFalse($this->adapter->createDir('file.txt/dir', new Config()));
@@ -101,12 +105,13 @@ class RedisAdapterTests extends \PHPUnit_Framework_TestCase
     {
         $meta = $this->adapter->getMetadata('file.txt');
 
-        $this->assertSame(5, count($meta));
+        $this->assertSame(6, count($meta));
         $this->assertSame('file.txt', $meta['path']);
         $this->assertSame('file', $meta['type']);
         $this->assertSame(8, $meta['size']);
         $this->assertSame('public', $meta['visibility']);
         $this->assertTrue(is_int($meta['timestamp']));
+        $this->assertSame('text/plain', $meta['mimetype']);
     }
 
     /**
@@ -169,27 +174,27 @@ class RedisAdapterTests extends \PHPUnit_Framework_TestCase
 
         $result = $this->adapter->listContents('', true);
         $this->assertSame(5, count($result));
-        $this->assertSame('file.txt', $result[0]['path']);
-        $this->assertSame('dir', $result[1]['path']);
-        $this->assertSame('dir/file.txt', $result[2]['path']);
-        $this->assertSame('dir/subdir', $result[3]['path']);
-        $this->assertSame('dir/subdir/file.txt', $result[4]['path']);
+        $this->assertSame('dir', $result[0]['path']);
+        $this->assertSame('dir/subdir', $result[1]['path']);
+        $this->assertSame('dir/subdir/file.txt', $result[2]['path']);
+        $this->assertSame('dir/file.txt', $result[3]['path']);
+        $this->assertSame('file.txt', $result[4]['path']);
 
         $result = $this->adapter->listContents('');
         $this->assertSame(2, count($result));
-        $this->assertSame('file.txt', $result[0]['path']);
-        $this->assertSame('dir', $result[1]['path']);
+        $this->assertSame('dir', $result[0]['path']);
+        $this->assertSame('file.txt', $result[1]['path']);
 
         $result = $this->adapter->listContents('dir', true);
         $this->assertSame(3, count($result));
-        $this->assertSame('dir/file.txt', $result[0]['path']);
-        $this->assertSame('dir/subdir', $result[1]['path']);
-        $this->assertSame('dir/subdir/file.txt', $result[2]['path']);
+        $this->assertSame('dir/subdir', $result[0]['path']);
+        $this->assertSame('dir/subdir/file.txt', $result[1]['path']);
+        $this->assertSame('dir/file.txt', $result[2]['path']);
 
         $result = $this->adapter->listContents('dir');
         $this->assertSame(2, count($result));
-        $this->assertSame('dir/file.txt', $result[0]['path']);
-        $this->assertSame('dir/subdir', $result[1]['path']);
+        $this->assertSame('dir/subdir', $result[0]['path']);
+        $this->assertSame('dir/file.txt', $result[1]['path']);
 
         $this->assertSame([], $this->adapter->listContents('no_dir'));
     }
@@ -220,8 +225,10 @@ class RedisAdapterTests extends \PHPUnit_Framework_TestCase
     public function testRename()
     {
         $this->assertTrue($this->adapter->rename('file.txt', 'dir/subdir/file.txt'));
+        $this->assertFalse($this->adapter->has('file.txt'));
         $this->assertTrue($this->adapter->has('dir'));
         $this->assertTrue($this->adapter->has('dir/subdir'));
+        $this->assertTrue($this->adapter->has('dir/subdir/file.txt'));
 
         $this->assertFalse($this->adapter->rename('dir/subdir/file.txt', 'dir/subdir/file.txt/new_file.txt'));
     }
@@ -250,15 +257,5 @@ class RedisAdapterTests extends \PHPUnit_Framework_TestCase
         $this->assertSame('new contents', $this->adapter->read('file.txt', new Config())['contents']);
 
         $this->assertFalse($this->adapter->update('new_file.txt', 'contents', new Config()));
-    }
-
-    /**
-     * @covers ::write
-     */
-    public function testWrite()
-    {
-        $result = $this->adapter->write('new_file.txt', 'new contents', new Config());
-        $this->assertSame('new contents', $result['contents']);
-        $this->assertFalse($this->adapter->write('file.txt/new_file.txt', 'contents', new Config()));
     }
 }
